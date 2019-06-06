dbc3476e190d3f8140c9dad9c20a46923af5447f
Fengyan Shi <fyshi@udel.edu>
2019-06-06 09:44:29 -0400

    add examples

--- simple_examples/beach_2d_str_sponge/simple_stretch.m
+++ simple_examples/beach_2d_str_sponge/simple_stretch.m
@@ -1,20 +1,24 @@
 clear all
 dxmin=0.5;
 dxmax=2.0;
-dxsponge=20.0;
+dxsponge=100.0;
 dymax=2.0;
 
 m=550;
 n=250;
 
-
-m_sponge=50;
-m_st=150;
+m_sponge_st=200;
+m_sponge=100;
+m_st=300;
 
 for i=1:m_sponge
-        dx(i)=dxsponge+(dxmax-dxsponge)*(i-1)/(m_sponge-1);
+        dx(i)=dxsponge;
 end
-for i=m_sponge+1:m_st
+
+for i=m_sponge+1:m_sponge_st
+        dx(i)=dxsponge+(dxmax-dxsponge)*(i-m_sponge)/(m_sponge_st-m_sponge);
+end
+for i=m_sponge_st+1:m_st
         dx(i)=dxmax;
 end
 for i=m_st+1:m
@@ -39,13 +43,13 @@ depmax=10.0;
 depmin=-2.0;
 dep=zeros(n,m)+depmax;
 
-for j=1:n
-    for i=1:m
-        if x(i)>xs
-        dep(j,i)=depmax+(x(i)-xs)*(depmin-depmax)/(x(m)-xs);
-        end
-    end
-end
+%for j=1:n
+ %   for i=1:m
+  %      if x(i)>xs
+   %     dep(j,i)=depmax+(x(i)-xs)*(depmin-depmax)/(x(m)-xs);
+    %    end
+   % end
+% end
 
 
 xx=x';
